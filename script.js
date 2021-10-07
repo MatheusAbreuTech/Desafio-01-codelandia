e9d2a766b7af45a9103cb76025061e1053175897
Matheus Abreu <theus.santosab@gmail.com>
2021-10-07 20:22:13 -0300

    add function liked

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const notices = [
     {
         date: '04 de ago, 2021',
-        title: 'Agora é oficial: o Windows 11 está vindo',
+        title: 'Tim Berners-Lee vai leiloar código-fonte da web',
         content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omni possimus veniam similique asperiores expedita totam excepturi nihil, perspiciatis ex velit rem numquam dolor dignissimos eius modi soluta iure ducimus ea.'
     },
     {
         date: '02 de jul, 2021',
-        title: 'Agora é oficial: o Windows 11 está vindo',
+        title: 'Tem Firefox novo no pedaço e você vai querer migrar',
         content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omni possimus veniam similique asperiores expedita totam excepturi nihil, perspiciatis ex velit rem numquam dolor dignissimos eius modi soluta iure ducimus ea.'
     },
     {
         date: '11 de jul, 2021',
-        title: 'Agora é oficial: o Windows 11 está vindo',
+        title: 'John McAfee, criador do antivírus McAfee, morre',
         content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omni possimus veniam similique asperiores expedita totam excepturi nihil, perspiciatis ex velit rem numquam dolor dignissimos eius modi soluta iure ducimus ea.'
     },
     {
@@ -36,7 +36,7 @@ const DOM = {
         <div class="box-notice">
             <div class="header-news">
                 <p class="news-date">${notice.date}</p>
-                <i class="far fa-heart"></i>
+                <i class="far fa-heart" data-like></i>
             </div>
             <h3 class="news-title">${notice.title}</h3>
             <p class="news-content">
@@ -45,12 +45,27 @@ const DOM = {
         </div>
         `
         return html
-    }
-}
+    },
 
-notices.forEach((notice) => {
-    DOM.addNotice(notice)
-})
+    liked() {
+        
+        let likes = document.querySelectorAll('[data-like]');
+
+        likes.forEach((like) => {
+            like.addEventListener("click", () => likedItem(like));
+        });
+
+        function likedItem(item) {
+            if (item.classList.contains('far')) {
+                item.classList.remove('far');
+                item.classList.add('fas')
+            } else {
+                item.classList.remove('fas');
+                item.classList.add('far')
+            }
+        }
+    },
+}
 
 const animate = () => {
     const sr = ScrollReveal({
@@ -63,4 +78,9 @@ const animate = () => {
     ScrollReveal().reveal('.box-notice', { delay: 200 });
 }
 
+notices.forEach((notice) => {
+    DOM.addNotice(notice)
+})
+
 animate()
+DOM.liked()
